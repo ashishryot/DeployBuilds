118d65178f39c172598fb43959582a4bff8940cf
ashishryot <ashishryot@gmail.com>
2021-03-03 19:28:40 -0600

    Fast copying Algo

--- DeployBuilds/Program.cs
+++ DeployBuilds/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections;
 
 namespace DeployBuilds
 {
@@ -8,7 +10,25 @@ namespace DeployBuilds
         {
             Console.WriteLine("*****Deploy Builds*****");
 
+            string[] dest_servs = File.ReadAllLines("servers.txt");
+            ArrayList src_servs = new ArrayList();
+            src_servs.Add("Main_Server");
 
+            int dest_serv_cntr = 0;
+
+            while (dest_serv_cntr < dest_servs.Length) {
+                ArrayList tmp_servs = new ArrayList();
+                foreach (string serv in src_servs) {
+                    Console.WriteLine(serv + " - " + dest_servs[dest_serv_cntr]);
+                    tmp_servs.Add(dest_servs[dest_serv_cntr]);
+                    dest_serv_cntr += 1;
+                    if(dest_serv_cntr >= dest_servs.Length)
+                    {
+                        break;
+                    }
+                }
+                src_servs.AddRange(tmp_servs);
+            }
 
             Console.ReadKey();
         }
